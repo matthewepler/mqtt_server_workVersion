78520fd26a45298f8520f883878d2c6e86ff2f68
Matthew epler <matthewepler@Matthews-MacBook-Pro.local>
2017-03-28 09:04:16 -0400

    initial commit all working

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+// static files
+import logo from './logo.svg'
+
+// styles
+import './App.css' // will load compiled css from ./App.scss
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
